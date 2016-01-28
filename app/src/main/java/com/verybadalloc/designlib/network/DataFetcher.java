83d196f41b1a5bcddff272cc29265802707c5481
Anas Ambri <anas.ambri@gmail.com>
2016-01-28 04:32:39 -0500

    API call to filter by book type

--- app/src/main/java/com/verybadalloc/designlib/network/DataFetcher.java
+++ app/src/main/java/com/verybadalloc/designlib/network/DataFetcher.java
@@ -3,6 +3,9 @@ package com.verybadalloc.designlib.network;
 import com.google.gson.Gson;
 import com.verybadalloc.designlib.model.Book;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aambri on 15-06-06.
  */
@@ -18,6 +21,31 @@ public class DataFetcher {
             public void onSuccess(String response) {
                 Book[] eateries = new Gson().fromJson(response, Book[].class);
                 callback.onSuccess(eateries);
+            }
+
+            @Override
+            public void onFailure(String reason) {
+                callback.onFailure(reason);
+            }
+        });
+    }
+
+    public static void getBooksOfType(final String type, final DataCallback<Book[]> callback) {
+
+        DataFetcher.getBooks(new DataCallback<Book[]>() {
+            @Override
+            public void onSuccess(Book[] data) {
+                if (!type.equalsIgnoreCase("All")) {
+                    List<Book> newList = new ArrayList<Book>(data.length);
+                    for (Book b : data) {
+                        if (b.type.equalsIgnoreCase(type)) {
+                            newList.add(b);
+                        }
+                    }
+                    callback.onSuccess(newList.toArray(new Book[newList.size()]));
+                } else {
+                    callback.onSuccess(data);
+                }
             }
 
             @Override
